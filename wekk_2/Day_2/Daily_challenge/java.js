b323470ab4500007db0cf6e539baa44a0098c41a
DONALD SANGO <sangooussoudoma@gmail.com>
2022-08-31 10:13:16 +0000

    dossier bootcamp créer

--- wekk_2/Day_2/Daily_challenge/java.js
+++
@@ -1,9 +0,0 @@
-/* exercice */
-let sentence="The movie is not that bad, I like it";
-let wordNot=sentence.indexOf("not");
-let wordBad=sentence.indexOf("bad");
-if(wordNot>wordBad){
-    console.log("The movie is good, I like it");
-}else{
-    console.log(sentence);
-}
